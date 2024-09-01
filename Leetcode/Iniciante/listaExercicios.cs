e12637626c269b2a23f90e9db897497055c245ca
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-09-01 15:50:44 -0300

    Estruturação

--- Leetcode/Iniciante/listaExercicios.cs
+++ Leetcode/Iniciante/listaExercicios.cs
@@ -8,34 +8,31 @@ namespace Leetcode.Iniciante
 {
     public class listaExercicios
     {
-        private readonly int[] nums  =  {1, 2, 3, 4, 5, 6, 7};
+        private readonly int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
         private readonly int target = 9;
         //construtor vazio.
         public listaExercicios() { }
 
         public void ExecuteMethods()
         {
-           Console.WriteLine(TwoSum(nums, target));
+            Console.WriteLine(TwoSum(nums, target));
         }
 
         #region Exercicio: 1 - TwoSum
-        public class Solution
+        public int[] TwoSum(int[] nums, int target)
         {
-            public int[] TwoSum(int[] nums, int target)
+            for (var i = 0; i < nums.Length; i++)
             {
-                for (var i = 0; i < nums.Length; i++)
+                for (var j = i + 1; j < nums.Length; j++)
                 {
-                    for (var j = i + 1; j < nums.Length; j++)
+                    var soma = nums[i] + nums[j];
+                    if (soma == target)
                     {
-                        var soma = nums[i] + nums[j];
-                        if (soma == target)
-                        {
-                            return new int[] { i, j };
-                        }
+                        return new int[] { i, j };
                     }
                 }
-                return new int[0];
             }
+            return new int[0];
         }
         #endregion
     }
