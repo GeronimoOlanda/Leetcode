d2bc8c6c88815b36137449430ccbb43f0937519a
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-09-01 13:35:52 -0300

    Ajuste no retorno da somatoria do J

--- Leetcode/Iniciante/listaExercicios.cs
+++ Leetcode/Iniciante/listaExercicios.cs
@@ -19,24 +19,23 @@ namespace Leetcode.Iniciante
         }
 
         #region Exercicio: 1 - TwoSum
-        public int[] TwoSum(int[] nums, int target)
+        public class Solution
         {
-            // Inicializa o array nums (removi a linha que redefinia nums, pois nums já é passado como argumento)
-            for (var i = 0; i < nums.Length; i++)
+            public int[] TwoSum(int[] nums, int target)
             {
-                for (var j = i + 1; j < nums.Length; j++)
-                { // Começa o segundo loop a partir de i + 1 para evitar somar o mesmo índice
-                    var soma = nums[i] + nums[j];
-                    if (soma == target)
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    for (var j = i + 1; j < nums.Length; j++)
                     {
-                        // Retorna um array contendo os índices i e j que satisfazem a condição
-                        return new int[] { i, j };
+                        var soma = nums[i] + nums[j];
+                        if (soma == target)
+                        {
+                            return new int[] { i, j };
+                        }
                     }
                 }
+                return new int[0];
             }
-
-            // Se nenhum par for encontrado, pode-se retornar um array vazio ou lançar uma exceção
-            return new int[0]; // ou throw new ArgumentException("Nenhum par encontrado que some o valor target.");
         }
         #endregion
     }
